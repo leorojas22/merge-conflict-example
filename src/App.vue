00b2ccbbb6a964e21f21b34a94f5ea76616ad420
Leo <leorojas22@gmail.com>
2019-02-10 20:28:50 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="col-6 offset-3 bg-dark p-4">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <input type="number" :value="value" readonly class="form-control-lg form-control">
+                    </div>
+                </div>
+            </div>
+            <app-key-row v-for="(keyRow, index) in keys" :key="index" :keyRow="keyRow" @input="calculate"></app-key-row>
+            <div class="row mb-4">
+                <div class="col-9 text-center">
+                    <button class="btn btn-lg btn-light btn-block" type="button" @click="calculate(0)">0</button>
+                </div>
+                <div class="col text-center">
+                    <button class="btn btn-lg btn-light btn-block" type="button" @click="calculate('/')">/</button>
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col offset-9 text-center">
+                    <button class="btn btn-lg btn-primary btn-block" type="button" @click="calculate('=')">=</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import KeyRow from './KeyRow.vue';
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    components: {
+        appKeyRow: KeyRow
+    },
+    data() {
+        return {
+            value: 0,
+            keys: [
+                [9, 8, 7, '+'],
+                [6, 5, 4, '-'],
+                [3, 2, 1, 'x'],
+            ]
+        }
+    },
+    methods: {
+        calculate(value) {
+            if(isNaN(value))
+            {
+                console.log("Action")
+            }
+            else
+            {
+                console.log("Number")
+
+                let newValue = "";
+                newValue += this.value + "" + value;
+                this.value = parseFloat(newValue);
+            }
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import url('https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css');
+
+    body {
+        padding: 20px 0px;
+    }
 </style>
---
+++ src/KeyRow.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row mb-4">
+        <div class="col text-center" v-for="key in keyRow" :key="key">
+            <button class="btn btn-lg btn-light btn-block" @click="buttonClicked(key)" type="button">{{ key }}</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'keyRow'
+    ],
+    methods: {
+        buttonClicked(value) {
+            this.$emit("input", value);
+        }
+    }
+}
+</script>
+
